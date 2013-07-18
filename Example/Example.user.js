0f044771b6fc6ffbc6b88cc9b05c650ef1a35ec2
simonsso <simonsso@renesasmobile.com>
2013-07-18 19:02:35 +0200

    Added qr generation

--- Example/Example.user.js
+++ Example/Example.user.js
@@ -21,9 +21,20 @@ function example(){
    else if (document.selection) {
       txt = document.selection.createRange().text;
    }
-   alert("example"+txt);
+	mainDoc = top.document.body;
+	image = "<div  id='imager' style='position:absolute;top:0px;left:0px;z-index:1000;border:black solid 10px;'><a href='#' onclick='javascript:imager=document.getElementById(\"imager\");imager.innerHTML=\"\"; ' ><div  style='border:white solid 10px;'><img src='http://qrcode.kaywa.com/img.php?s=8&d=" +   txt + "' alt='qrcode' id='qrcodethingy' style='border:black solid 0px;'/></div></a></div>";
+	
+	mainDoc.innerHTML = image + mainDoc.innerHTML;
+	imagediv = document.getElementById("imager");
+	
+	if (imagediv){
+		imagediv.style.visibility = "visible";				
+	}
+
+   alert(txt);
 }
 
    GM_registerMenuCommand("Hello, world! (example)", example, "e" );
-   // uncoment for debugging alert("Hello world");
+   // uncoment for debugging 
+	alert("Hello world");
 //};
