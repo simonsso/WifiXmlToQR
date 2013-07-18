c1db3aaf4341d0a7703805912fbf4cdbdd83214e
simonsso <simonsso@renesasmobile.com>
2013-07-19 01:08:46 +0200

    added some xml parsing

--- Example/Example.user.js
+++ Example/Example.user.js
@@ -21,20 +21,36 @@ function example(){
    else if (document.selection) {
       txt = document.selection.createRange().text;
    }
-	mainDoc = top.document.body;
-	image = "<div  id='imager' style='position:absolute;top:0px;left:0px;z-index:1000;border:black solid 10px;'><a href='#' onclick='javascript:imager=document.getElementById(\"imager\");imager.innerHTML=\"\"; ' ><div  style='border:white solid 10px;'><img src='http://qrcode.kaywa.com/img.php?s=8&d=" +   txt + "' alt='qrcode' id='qrcodethingy' style='border:black solid 0px;'/></div></a></div>";
-	
-	mainDoc.innerHTML = image + mainDoc.innerHTML;
-	imagediv = document.getElementById("imager");
-	
-	if (imagediv){
-		imagediv.style.visibility = "visible";				
-	}
-
    alert(txt);
+   //TODO edit the xpression something like this txt.replace(/""/g,'"');
+   alert(txt);
+   var z=parseXml(txt);
+   var datastr=z.getElementsByTagName("SSID");
+   var aa=z.getElementsByTagName("SSIDConfig")[0];
+   var aaa=aa.childNodes.item("SSID");
+   var aaa=aa.childNodes.item("SSID").childNodes("hex");
+   alert(aa.childNodes("SSID").childNodes("hex").textContent);
+   return;
+   mainDoc = top.document.body;
+   image = "<div  id='imager' style='position:absolute;top:0px;left:0px;z-index:1000;border:black solid 10px;'><a href='#' onclick='javascript:imager=document.getElementById(\"imager\");imager.innerHTML=\"\"; ' ><div  style='border:white solid 10px;'><img src='http://qrcode.kaywa.com/img.php?s=8&d=" +   txt + "' alt='qrcode' id='qrcodethingy' style='border:black solid 0px;'/></div></a></div>";
+	
+   mainDoc.innerHTML = image + mainDoc.innerHTML;
+   imagediv = document.getElementById("imager");
+	
+   if (imagediv){
+      imagediv.style.visibility = "visible";				
+   }
 }
+   if (typeof window.DOMParser != "undefined") {
+	   parseXml = function(xmlStr) {
+		return ( new window.DOMParser() ).parseFromString(xmlStr, "text/xml");
+	   };
+   }else{
+   	alert("Did not find xml parser");
+   };
 
    GM_registerMenuCommand("Hello, world! (example)", example, "e" );
    // uncoment for debugging 
-	alert("Hello world");
+   alert("All systems loaded");
+
 //};
