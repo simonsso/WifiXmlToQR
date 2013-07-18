13b48f2176b1951a65baa351feb31b4a871ecea5
simonsso <simonsso@renesasmobile.com>
2013-07-18 18:40:05 +0200

    reorder and reformat

--- Example/Example.user.js
+++ Example/Example.user.js
@@ -2,7 +2,8 @@
 // ==UserScript==
 // @name        Example
 // @description example
-// @include http://www.murloc.org/*
+// @include http://www.murloc.org/alts.html
+// @include http://www.murloc.org/
 // @namespace     http://localhost.localdomain
 // @version     1
 // ==/UserScript==
@@ -10,19 +11,19 @@
 //GM_log("hello world");
 //GM_openInTab("http://www.murloc.org");
 function example(){
- var txt="xxx";
- if (window.getSelection) {
-    txt = window.getSelection();
-  }
-  else if (document.getSelection) {
-    txt = document.getSelection();
-  }
-  else if (document.selection) {
-    txt = document.selection.createRange().text;
-  }
- alert("example"+txt);
+   var txt="xxx";
+   if (window.getSelection) {
+      txt = window.getSelection();
+   }
+   else if (document.getSelection) {
+      txt = document.getSelection();
+   }
+   else if (document.selection) {
+      txt = document.selection.createRange().text;
+   }
+   alert("example"+txt);
 }
 
-   alert("Hello world");
-   GM_registerMenuCommand("Hello, world! (example)", example, "e", "shift alt", "w");
+   GM_registerMenuCommand("Hello, world! (example)", example, "e" );
+   // uncoment for debugging alert("Hello world");
 //};
